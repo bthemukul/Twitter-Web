aba75b3d7dc2e8c3907671ffece7f6f2818c0f17
Mukul Bisen <bthemukul@users.noreply.github.com>
2018-12-24 17:01:37 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-
+//Event Listener
+addEventListener();
+function addEventListener(){
+    document.querySelector('form').addEventListener('submit', tweetSave);
+}
+function tweetSave(e){
+e.preventDefault();
+console.log("Tweet Submitted");
+}

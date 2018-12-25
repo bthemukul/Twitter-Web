6324095cb2e50e703d75916771e1cba2e48fac45
Mukul Bisen <bthemukul@users.noreply.github.com>
2018-12-25 22:08:52 +0530

    Updating the tweet fetch and remove button in code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,28 @@
+//Global Variable tweetList declaration
+const tweetList = document.getElementById(`tweet-list`);
+
 //Event Listener
 addEventListener();
 function addEventListener(){
     document.querySelector('form').addEventListener('submit', tweetSave);
 }
+
+//Main function
 function tweetSave(e){
-e.preventDefault();
-console.log("Tweet Submitted");
+    e.preventDefault();
+    const tweet = document.getElementById(`tweet`).value;
+    
+    //Creating remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
+    //create li element and appending it in tweetList
+    const li = document.createElement('li');
+    li.textcontent = tweet;
+    tweetList.appentChild(li);
+
+    //Adding remove button in each tweet
+    li.appendChild(removeBtn);
+    
 }
